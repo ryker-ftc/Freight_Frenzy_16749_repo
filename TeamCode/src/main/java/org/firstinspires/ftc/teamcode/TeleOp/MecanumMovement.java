ba787bf603ab8fc98e2a31a4afbf9bc24215ea2f
Vivaan Shah <shahvivaan1508@gmail.con>
2021-10-31 13:26:08 -0400

    Added intake code to the other mecanum drive code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MecanumMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MecanumMovement.java
@@ -48,15 +48,21 @@ public class MecanumMovement extends LinearOpMode {
     public void runOpMode() {
         double x1 = 0; // left and right
         double y1 = 0; // front and back
-
+        double x2 = 0; // left and right + 45 deg. fix.
+        double y2 = 0; // front and back + 45 deg. fix.
         double fortyFiveInRads = -Math.PI/4;
         double cosine45 = Math.cos(fortyFiveInRads);
         double sine45 = Math.sin(fortyFiveInRads);
 
-        double x2 = 0; // left and right + 45 deg. fix.
-        double y2 = 0; // front and back + 45 deg. fix.
+        //Servo Settings
 
-
+        //Intake Servo settings
+        double intakeSpeed = 0.03;
+        double intakeClaw_HOME = 0.2;// Starting Postion of the servo.
+        double intakeClaw_MIN_RANGE = 0.0;//Minimum range of the servo.
+        double intakeClaw_MAX_RANGE = 0.7;//Maximum range of the servo.
+        double intakeClaw_SPEED = 0.1;//Servo speed
+        double intakeClaw_POSITION = intakeClaw_HOME;//Servo speed
 
         robot.init(hardwareMap);
         waitForStart();
@@ -74,6 +80,7 @@ public class MecanumMovement extends LinearOpMode {
                 robot.backLeftMotor.setPower(spin);
             }
             //Drive
+
             //getting the y value of the joystick(I put a negative because the joystick is flipped.)
             y1 = -gamepad1.left_stick_y;
             //getting the x value of the joystick
@@ -92,7 +99,23 @@ public class MecanumMovement extends LinearOpMode {
             telemetry.addData("x1",  "%.2f", x2);
             telemetry.addData("y1", "%.2f", y2);
             telemetry.update();
+
             //Drive end
+
+            //Intake
+            intakeClaw_POSITION = Range.clip(intakeClaw_POSITION,intakeClaw_MIN_RANGE,intakeClaw_MAX_RANGE);
+            if(gamepad1.x) {
+                intakeClaw_POSITION += intakeClaw_SPEED;
+            }else{
+                intakeClaw_POSITION -= intakeClaw_SPEED;
+            }
+
+            if(gamepad1.dpad_up) {
+                robot.intakeMotor.setPower(intakeSpeed);
+            }else if(gamepad1.dpad_down){
+                robot.intakeMotor.setPower(-intakeSpeed);
+
+            }
 
 
         }
