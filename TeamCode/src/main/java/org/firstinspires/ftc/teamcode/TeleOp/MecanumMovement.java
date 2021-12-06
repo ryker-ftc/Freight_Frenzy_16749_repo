d6df8a9429d051cef027e72d5523caf215ec6df4
Vivaan Shah <shahvivaan1508@gmail.con>
2021-12-06 16:47:21 -0500

    ---

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MecanumMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MecanumMovement.java
@@ -2,16 +2,67 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 @TeleOp(name = "Mecanum2.0", group = "Tele-op")
 public class MecanumMovement extends LinearOpMode {
+    //Motors
+    public DcMotor  frontLeftMotor= null;
+    public DcMotor  frontRightMotor= null;
+    public DcMotor  backLeftMotor= null;
+    public DcMotor  backRightMotor= null;
+    public DcMotor intakeMotor = null;
+    public DcMotor duckSpinner = null;
+    public Servo intakeClaw = null;
 
+    //Servos
+    //public Servo intakeClaw = null;
+
+    /* local OpMode members. */
+    HardwareMap hwMap =  null;
     /* Declare OpMode members. */
-    SetupClass robot = new SetupClass();
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        backLeftMotor= hwMap.get(DcMotor.class, "m0");
+        frontLeftMotor  = hwMap.get(DcMotor.class, "m1");
+        backRightMotor= hwMap.get(DcMotor.class, "m2");
+        frontRightMotor = hwMap.get(DcMotor.class, "m3");
+        duckSpinner = hwMap.get(DcMotor.class, "mx0");
+        intakeMotor = hwMap.get(DcMotor.class, "mx1");
+        // Define and Initialize Servos
+
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        intakeMotor.setDirection(DcMotor.Direction.FORWARD);
+        // Set all motors to zero power
+        frontLeftMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backRightMotor.setPower(0);
+        intakeMotor.setPower(0);
+        duckSpinner.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        duckSpinner.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
 
 
-    @Override
+
     public void runOpMode() {
 
 //        final double INTAKE_HOME = 0.0;
@@ -30,23 +81,23 @@ public class MecanumMovement extends LinearOpMode {
             double frontRightMotorSpeed = -left_stick_x + left_stick_y + spin;
             double backRightMotorSpeed = left_stick_x + left_stick_y + spin;
 
-            robot.backLeftMotor.setPower(backLeftMotorSpeed);
-            robot.frontLeftMotor.setPower(frontLeftMotorSpeed);
-            robot.frontRightMotor.setPower(frontRightMotorSpeed);
-            robot.backRightMotor.setPower(backRightMotorSpeed);
+            backLeftMotor.setPower(backLeftMotorSpeed);
+            frontLeftMotor.setPower(frontLeftMotorSpeed);
+            frontRightMotor.setPower(frontRightMotorSpeed);
+            backRightMotor.setPower(backRightMotorSpeed);
 
             if (gamepad1.a) {
-                robot.duckSpinner.setPower(0.7);
+                duckSpinner.setPower(0.7);
             } else {
-                robot.duckSpinner.setPower(0.0);
+                duckSpinner.setPower(0.0);
             }
 
             if (gamepad1.dpad_up) {
-                robot.intakeMotor.setPower(0.08);
+                intakeMotor.setPower(0.08);
             } else if (gamepad1.dpad_down) {
-                robot.intakeMotor.setPower(-0.08);
+                intakeMotor.setPower(-0.08);
             } else {
-                robot.intakeMotor.setPower(0);
+                intakeMotor.setPower(0);
             }
 //            if (gamepad1.x) {
 //                INTAKE_POSITION += INTAKE_SPEED;
